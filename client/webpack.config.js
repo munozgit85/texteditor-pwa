420f067dec4a81d8f6afc63c2a2db708d3500427
Racquel Munoz <racquelg85@hotmail.com>
2022-11-21 12:45:43 -0600

    added other dependency

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -58,12 +58,20 @@ module.exports = () => {
           type: "asset/resource",
         },
         {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
             loader: "babel-loader",
             options: {
-              presets: [["@babel/preset-env", { targets: "defaults" }]],
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
